3a6847539b2bcb5155ffb030dbccb8e86af4e316
Fenil Shingala <fenil.shingala@digipen.edu>
2019-10-14 21:28:47 -0700

    basic shadows

--- Phoenix/RendererOpenGL/App/ForwardLighting.cpp
+++ Phoenix/RendererOpenGL/App/ForwardLighting.cpp
@@ -5,7 +5,24 @@
 #include "../Common.h"
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(-10.0f, 8.0f, -10.0f);
+
+OpenGLRenderer* pOpenGLRenderer = NULL;
+
+void RenderScene(ShaderProgram& shader)
+{
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::scale(model, glm::vec3(10.0f, 0.5f, 10.0f));
+	shader.SetUniform("model", &model);
+
+	pOpenGLRenderer->RenderCube();
+
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f));
+	shader.SetUniform("model", &model);
+
+	pOpenGLRenderer->RenderCube();
+}
 
 void Run()
 {
@@ -13,94 +30,52 @@ void Run()
 	lastX = window.windowWidth()  / 2.0f;
 	lastY = window.windowHeight() / 2.0f;
 
+	camera.Position = glm::vec3(0.0f, 2.0f, 10.0f);
+
+	pOpenGLRenderer = new OpenGLRenderer();
+
 	glEnable(GL_DEPTH_TEST);
-	
+	glEnable(GL_MULTISAMPLE);
+
 	ShaderProgram lightingShader("../../Phoenix/RendererOpenGL/App/Resources/Shaders/basic_lighting.vert",
 								 "../../Phoenix/RendererOpenGL/App/Resources/Shaders/basic_lighting.frag");
 	ShaderProgram lampShader("../../Phoenix/RendererOpenGL/App/Resources/Shaders/mesh.vert",
 							 "../../Phoenix/RendererOpenGL/App/Resources/Shaders/mesh.frag");
 
-	float vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	ShaderProgram simpleDepthShader("../../Phoenix/RendererOpenGL/App/Resources/Shaders/shadow_depth.vert",
+								    "../../Phoenix/RendererOpenGL/App/Resources/Shaders/shadow_depth.frag");
 
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-	};
-	// first, configure the cube's VAO (and VBO)
-	unsigned int VBO, cubeVAO;
-	glGenVertexArrays(1, &cubeVAO);
-	glGenBuffers(1, &VBO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindVertexArray(cubeVAO);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// normal attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	ShaderProgram debugDepthQuad("../../Phoenix/RendererOpenGL/App/Resources/Shaders/shadow_debug.vert",
+								 "../../Phoenix/RendererOpenGL/App/Resources/Shaders/shadow_debug.frag");
+	// configure depth map FBO
+	// -----------------------
+	const unsigned int SHADOW_WIDTH = 2048, SHADOW_HEIGHT = 2048;
+	unsigned int depthMapFBO;
+	glGenFramebuffers(1, &depthMapFBO);
+	// create depth texture
+	unsigned int depthMap;
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// attach depth texture as FBO's depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 
-	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
-	unsigned int lightVAO;
-	glGenVertexArrays(1, &lightVAO);
-	glBindVertexArray(lightVAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// note that we update the lamp's position attribute's stride to reflect the updated buffer data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	//uint32_t texture0 = LoadTexture("../../Phoenix/App/Test/textures/container.jpg");
-	//uint32_t texture1 = LoadTexture("../../Phoenix/App/Test/textures/awesomeface.png");
-	
-	//int zero = 0, one = 1;
-	//glUseProgram(shaderProgram.mId);
-	//shaderProgram.SetUniform("texture1", &zero);
-	//shaderProgram.SetUniform("texture2", &one);
-
-	//glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)window.windowWidth() / (float)window.windowHeight(), 0.1f, 100.0f);
-	//shaderProgram.SetUniform("projection", &projection);
+	// shader configuration
+	// --------------------
+	int zero = 0;
+	glUseProgram(lightingShader.mId);
+	lightingShader.SetUniform("shadowMap", &zero);
+	glUseProgram(debugDepthQuad.mId);
+	debugDepthQuad.SetUniform("depthMap", &zero);
 
 	window.initGui();
 
@@ -109,44 +84,85 @@ void Run()
 		window.startFrame();
 
 		{
+			float near_plane = 0.1f, far_plane = 100.0f;
+		
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)window.windowWidth() / (float)window.windowHeight(), near_plane, far_plane);
+			glm::mat4 view = camera.GetViewMatrix();
+
 			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+			// 1. render depth of scene to texture (from light's perspective)
+			// --------------------------------------------------------------
+			glm::mat4 lightProjection, lightView;
+			glm::mat4 lightSpaceMatrix;
+			lightProjection  = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+			lightView	     = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+			lightSpaceMatrix = lightProjection * lightView;
+			
+			// render scene from light's point of view
+			glUseProgram(simpleDepthShader.mId);
+			simpleDepthShader.SetUniform("lightSpaceMatrix", &lightSpaceMatrix);
 
-			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)window.windowWidth() / (float)window.windowHeight(), 0.1f, 100.0f);
-			glm::mat4 view = camera.GetViewMatrix();
+			glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+
+			glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+			
+			glClear(GL_DEPTH_BUFFER_BIT);
+			glCullFace(GL_FRONT);
+			RenderScene(simpleDepthShader);
+			glCullFace(GL_BACK);
+
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			
+
+			// reset viewport
+			glViewport(0, 0, window.windowWidth(), window.windowHeight());
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			// draw our first triangle
 			glUseProgram(lightingShader.mId);
+
 			glm::vec3 objectColor = glm::vec3(1.0f, 0.5f, 0.31f);
 			lightingShader.SetUniform("objectColor", &objectColor);
 			glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
 			lightingShader.SetUniform("lightColor", &lightColor);
 			lightingShader.SetUniform("lightPos", &lightPos);
 			lightingShader.SetUniform("viewPos", &camera.Position);
+			lightingShader.SetUniform("lightSpaceMatrix", &lightSpaceMatrix);
 
 			lightingShader.SetUniform("projection", &projection);
 			lightingShader.SetUniform("view", &view);
 
-			glm::mat4 model = glm::mat4(1.0f);
-			lightingShader.SetUniform("model", &model);
-
-			// render the cube
-			glBindVertexArray(cubeVAO);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, depthMap);
+			RenderScene(lightingShader);
 
 			// also draw the lamp object
 			glUseProgram(lampShader.mId);
 
 			lampShader.SetUniform("projection", &projection);
 			lampShader.SetUniform("view", &view);
-			model = glm::mat4(1.0f);
+			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, lightPos);
 			model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
 			lampShader.SetUniform("model", &model);
 
-			glBindVertexArray(lightVAO);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
+			pOpenGLRenderer->RenderCube();
+
+			// render Depth map to quad for visual debugging
+			// ---------------------------------------------
+			
+			int xOffset = window.windowWidth()  - window.windowWidth() / 6  - 10;
+			int yOffset = window.windowHeight() - window.windowHeight() / 6 - 10;
+			glViewport(xOffset, yOffset, window.windowWidth()/6, window.windowHeight()/6);
+
+			glUseProgram(debugDepthQuad.mId);
+			debugDepthQuad.SetUniform("near_plane", &near_plane);
+			debugDepthQuad.SetUniform("far_plane", &far_plane);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, depthMap);
+			pOpenGLRenderer->RenderQuad();
 
 			// GUI
 			{
@@ -176,9 +192,9 @@ void Run()
 
 	window.exitGui();
 
-	glDeleteVertexArrays(1, &cubeVAO);
+	/*glDeleteVertexArrays(1, &cubeVAO);
 	glDeleteVertexArrays(1, &lightVAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &VBO);*/
 
 	window.exitWindow();
 }
--- Phoenix/RendererOpenGL/App/Resources/Shaders/basic_lighting.frag
+++ Phoenix/RendererOpenGL/App/Resources/Shaders/basic_lighting.frag
@@ -1,13 +1,38 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;  
-in vec3 FragPos;  
-  
+//in vec3 Normal;
+//in vec3 FragPos;
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec4 FragPosLightSpace;
+} fs_in;
+
+//uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
+
 uniform vec3 lightPos; 
 uniform vec3 viewPos; 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    // perform perspective divide
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    // transform to [0,1] range
+    projCoords = projCoords * 0.5 + 0.5;
+	// get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+    // get depth of current fragment from light's perspective
+    float currentDepth = projCoords.z;
+    // check whether current frag pos is in shadow
+	float bias = 0.005;
+    float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
+    return shadow;
+}
 
 void main()
 {
@@ -16,18 +41,20 @@ void main()
     vec3 ambient = ambientStrength * lightColor;
   	
     // diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 norm = normalize(fs_in.Normal);
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
     
     // specular
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    vec3 specular = specularStrength * spec * lightColor;
+
+	float shadow = ShadowCalculation(fs_in.FragPosLightSpace);
         
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = (ambient + (1.0 - shadow) * (diffuse + specular)) * objectColor;
     FragColor = vec4(result, 1.0);
-} 
+}
